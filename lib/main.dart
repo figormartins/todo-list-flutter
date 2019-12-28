2a997ae0aeab466fcd79791efebcd1b4a88d53d7
Igor Martins <figortmartins@gmail.com>
2019-12-28 00:00:54 -0300

    Lendo, escrevendo, deletando e ordenando os itens da lista no arquivo.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 
 void main() => runApp(
@@ -17,8 +16,21 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List _todoList = [];
   TextEditingController _todoController = TextEditingController();
+  List _todoList = [];
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _readData().then((data) {
+      setState(() {
+        _todoList = json.decode(data);
+      });
+    });
+  }
 
   void _addToDo() {
     Map<String, dynamic> newToDo = Map<String, dynamic>();
@@ -28,8 +40,28 @@ class _HomeState extends State<Home> {
       newToDo["isChecked"] = false;
       _todoController.text = "";
       _todoList.add(newToDo);
+      _saveData();
+    });
+  }
+
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(milliseconds: 1500));
+
+    setState(() {
+      _todoList.sort((a, b) {
+        if (a["isChecked"] && !b["isChecked"]) {
+          return 1;
+        } else if (!a["isChecked"] && b["isChecked"]) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+
+      _saveData();
     });
 
+    return null;
   }
 
   @override
@@ -59,42 +91,83 @@ class _HomeState extends State<Home> {
                   ),
                 ),
                 RaisedButton(
-                  color: Colors.blueAccent,
-                  child: Text("Add"),
-                  textColor: Colors.white,
-                  onPressed: _addToDo
-                )
+                    color: Colors.blueAccent,
+                    child: Text("Add"),
+                    textColor: Colors.white,
+                    onPressed: _addToDo)
               ],
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.only(top: 5.0),
-              itemCount: _todoList.length,
-              itemBuilder: (context, index) {
-                return CheckboxListTile(
-                  title: Text(_todoList[index]["title"]),
-                  value: _todoList[index]["isChecked"],
-                  secondary: CircleAvatar(
-                    backgroundColor: Colors.white12,
-                    child: Icon(
-                      _todoList[index]["isChecked"]
-                          ? Icons.check
-                          : Icons.hdr_weak,
-                    ),
-                  ),
-                  onChanged: (isChecked) {
-                    print(isChecked);
-                    setState(() {
-                      _todoList[index]["isChecked"] = isChecked;
-                    });
-                  },
-                );
-              },
+            child: RefreshIndicator(
+              onRefresh: _refresh,
+              child: ListView.builder(
+                padding: EdgeInsets.only(top: 5.0),
+                itemCount: _todoList.length,
+                itemBuilder: buildItem,
+              ),
             ),
           ),
         ],
       ),
+    );
+  }
+
+  Widget buildItem(context, index) {
+    return Dismissible(
+      key: Key(index.toString()),
+      background: Container(
+        color: Colors.redAccent[400],
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_todoList[index]["title"]),
+        value: _todoList[index]["isChecked"],
+        secondary: CircleAvatar(
+          backgroundColor: Colors.white12,
+          child: Icon(
+            _todoList[index]["isChecked"] ? Icons.check : Icons.hdr_weak,
+          ),
+        ),
+        onChanged: (isChecked) {
+          print(isChecked);
+          setState(() {
+            _todoList[index]["isChecked"] = isChecked;
+            _saveData();
+          });
+        },
+      ),
+      onDismissed: (direction) {
+        setState(() async {
+          _lastRemoved = Map.from(_todoList[index]);
+          _lastRemovedPos = index;
+          _todoList.removeAt(index);
+          await _saveData();
+
+          final snack = SnackBar(
+            content: Text("Tarefa ${_lastRemoved["title"]} removida"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: () {
+                setState(() {
+                  _todoList.insert(_lastRemovedPos, _lastRemoved);
+                  _saveData();
+                });
+              },
+            ),
+            duration: Duration(seconds: 3),
+          );
+
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
     );
   }
 
